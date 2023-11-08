93356470de5d4d66ec8cb8322737af277884f774
usdt111 <vsd219772@gmail.com>
2023-11-08 18:30:44 +0300

    FirSt

--- start.py
+++ start.py
@@ -2,3 +2,21 @@ def vowel_indices(word):
     "Задача на CodeWars"
     vowels = 'aeiouAEIOU'
     return [index + 1 for index, letter in enumerate(word) if letter in vowels]
+#_________________________________________________________________________________
+
+
+def make_upper_case(s):
+    return s.upper()
+
+
+#____________________________________________________________________________________
+
+
+
+def power_of_two(x):
+    if x == 0:
+        return False
+    if x & (x - 1) == 0:
+        return True
+    else:
+        return False
