8e46e271ab53ff387c88bff477e0a7168789aa18
usdt111 <vsd219772@gmail.com>
2023-12-11 21:29:19 +0300

    first

--- start.py
+++ start.py
@@ -191,3 +191,11 @@ def remove_url_anchor(url):
         else:
             a.append(i)
     return ''.join(map(str, a))
+
+#____________________________________________________________________________________
+
+def is_leap_year(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
